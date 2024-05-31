6360f3f4bcd25250a6e9691ce7e891406e8fd064
karimovsardorbek <karimovsardor206@gmail.com>
2024-05-31 15:32:54 +0500

    fixed api

--- back/weather/views.py
+++ back/weather/views.py
@@ -5,7 +5,7 @@ import requests
 @api_view(['GET'])
 def get_weather(request):
     city = request.GET.get('city', 'New York')  # Default to New York if no city provided
-    api_key = '0258248a2f493261f57a28c34635480c'
+    api_key = 'put_weather_api_yourself'
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
 
     response = requests.get(url)
